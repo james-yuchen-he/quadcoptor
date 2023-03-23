49412afca8a7e71857a199a31e3750475719ac10
James He <james.he@uwaterloo.ca>
2023-03-22 22:41:23 -0400

    macro define 1 as high, 0 as low. fixed the corresponding reference in code as well

--- YMFC-AL/YMFC-AL_Flight_controller/YMFC-AL_Flight_controller.ino
+++ YMFC-AL/YMFC-AL_Flight_controller/YMFC-AL_Flight_controller.ino
@@ -18,6 +18,9 @@
 #include <Wire.h>                          //Include the Wire.h library so we can communicate with the gyro.
 #include <EEPROM.h>                        //Include the EEPROM.h library so we can store information onto the EEPROM
 
+#define HIGH 1
+#define LOW 0
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //PID gain and limit settings
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -360,47 +363,47 @@ ISR(PCINT0_vect){
   current_time = micros();
   //Channel 1=========================================
   if(PINB & B00000001){                                                     //Is input 8 high?
-    if(last_channel_1 == 0){                                                //Input 8 changed from 0 to 1.
-      last_channel_1 = 1;                                                   //Remember current input state.
+    if(last_channel_1 == LOW){                                                //Input 8 changed from 0 to 1.
+      last_channel_1 = HIGH;                                                   //Remember current input state.
       timer_1 = current_time;                                               //Set timer_1 to current_time.
     }
   }
-  else if(last_channel_1 == 1){                                             //Input 8 is not high and changed from 1 to 0.
-    last_channel_1 = 0;                                                     //Remember current input state.
+  else if(last_channel_1 == HIGH){                                             //Input 8 is not high and changed from 1 to 0.
+    last_channel_1 = LOW;                                                     //Remember current input state.
     receiver_input[1] = current_time - timer_1;                             //Channel 1 is current_time - timer_1.
   }
   //Channel 2=========================================
   if(PINB & B00000010 ){                                                    //Is input 9 high?
-    if(last_channel_2 == 0){                                                //Input 9 changed from 0 to 1.
-      last_channel_2 = 1;                                                   //Remember current input state.
+    if(last_channel_2 == LOW){                                                //Input 9 changed from 0 to 1.
+      last_channel_2 = HIGH;                                                   //Remember current input state.
       timer_2 = current_time;                                               //Set timer_2 to current_time.
     }
   }
-  else if(last_channel_2 == 1){                                             //Input 9 is not high and changed from 1 to 0.
-    last_channel_2 = 0;                                                     //Remember current input state.
+  else if(last_channel_2 == HIGH){                                             //Input 9 is not high and changed from 1 to 0.
+    last_channel_2 = LOW;                                                     //Remember current input state.
     receiver_input[2] = current_time - timer_2;                             //Channel 2 is current_time - timer_2.
   }
   //Channel 3=========================================
   if(PINB & B00000100 ){                                                    //Is input 10 high?
-    if(last_channel_3 == 0){                                                //Input 10 changed from 0 to 1.
-      last_channel_3 = 1;                                                   //Remember current input state.
+    if(last_channel_3 == LOW){                                                //Input 10 changed from 0 to 1.
+      last_channel_3 = HIGH;                                                   //Remember current input state.
       timer_3 = current_time;                                               //Set timer_3 to current_time.
     }
   }
-  else if(last_channel_3 == 1){                                             //Input 10 is not high and changed from 1 to 0.
-    last_channel_3 = 0;                                                     //Remember current input state.
+  else if(last_channel_3 == HIGH){                                             //Input 10 is not high and changed from 1 to 0.
+    last_channel_3 = LOW;                                                     //Remember current input state.
     receiver_input[3] = current_time - timer_3;                             //Channel 3 is current_time - timer_3.
 
   }
   //Channel 4=========================================
   if(PINB & B00001000 ){                                                    //Is input 11 high?
-    if(last_channel_4 == 0){                                                //Input 11 changed from 0 to 1.
-      last_channel_4 = 1;                                                   //Remember current input state.
+    if(last_channel_4 == LOW){                                                //Input 11 changed from 0 to 1.
+      last_channel_4 = HIGH;                                                   //Remember current input state.
       timer_4 = current_time;                                               //Set timer_4 to current_time.
     }
   }
-  else if(last_channel_4 == 1){                                             //Input 11 is not high and changed from 1 to 0.
-    last_channel_4 = 0;                                                     //Remember current input state.
+  else if(last_channel_4 == HIGH){                                             //Input 11 is not high and changed from 1 to 0.
+    last_channel_4 = LOW;                                                     //Remember current input state.
     receiver_input[4] = current_time - timer_4;                             //Channel 4 is current_time - timer_4.
   }
 }
