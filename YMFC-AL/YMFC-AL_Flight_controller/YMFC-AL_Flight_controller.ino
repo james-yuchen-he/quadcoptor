56fa491a09dd5ac2b000ec8a3dc3696aad9f2783
James He <james.he@uwaterloo.ca>
2023-03-22 23:00:05 -0400

    more renaming

--- YMFC-AL/YMFC-AL_Flight_controller/YMFC-AL_Flight_controller.ino
+++ YMFC-AL/YMFC-AL_Flight_controller/YMFC-AL_Flight_controller.ino
@@ -1,4 +1,4 @@
- ///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 //Terms of use
 ///////////////////////////////////////////////////////////////////////////////////////
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -49,7 +49,7 @@ byte eeprom_data[36];
 byte highByte, lowByte;
 volatile int receiver_input_channel_1, receiver_input_channel_2, receiver_input_channel_3, receiver_input_channel_4;
 int counter_channel_1, counter_channel_2, counter_channel_3, counter_channel_4, loop_counter;
-int esc_1, esc_2, esc_3, esc_4;
+int esc_1_cmd, esc_2_cmd, esc_3_cmd, esc_4_cmd;
 int throttle;
 float battery_voltage;
 int cal_int, start, gyro_address;
@@ -285,35 +285,35 @@ void loop(){
 
   if (start == 2){                                                          //The motors are started.
     if (throttle > 1800) throttle = 1800;                                   //We need some room to keep full control at full throttle.
-    esc_1 = throttle - pid_output_pitch + pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 1 (front-right - CCW)
-    esc_2 = throttle + pid_output_pitch + pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 2 (rear-right - CW)
-    esc_3 = throttle + pid_output_pitch - pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 3 (rear-left - CCW)
-    esc_4 = throttle - pid_output_pitch - pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 4 (front-left - CW)
+    esc_1_cmd = throttle - pid_output_pitch + pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 1 (front-right - CCW)
+    esc_2_cmd = throttle + pid_output_pitch + pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 2 (rear-right - CW)
+    esc_3_cmd = throttle + pid_output_pitch - pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 3 (rear-left - CCW)
+    esc_4_cmd = throttle - pid_output_pitch - pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 4 (front-left - CW)
 
     // Turning off voltage compensation for now.
     // if (battery_voltage < 1240 && battery_voltage > 800){                   //Is the battery connected?
-    //   esc_1 += esc_1 * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-1 pulse for voltage drop.
-    //   esc_2 += esc_2 * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-2 pulse for voltage drop.
-    //   esc_3 += esc_3 * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-3 pulse for voltage drop.
-    //   esc_4 += esc_4 * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-4 pulse for voltage drop.
+    //   esc_1_cmd += esc_1_cmd * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-1 pulse for voltage drop.
+    //   esc_2_cmd += esc_2_cmd * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-2 pulse for voltage drop.
+    //   esc_3_cmd += esc_3_cmd * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-3 pulse for voltage drop.
+    //   esc_4_cmd += esc_4_cmd * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-4 pulse for voltage drop.
     // }
 
-    if (esc_1 < 1100) esc_1 = 1100;                                         //Keep the motors running.
-    if (esc_2 < 1100) esc_2 = 1100;                                         //Keep the motors running.
-    if (esc_3 < 1100) esc_3 = 1100;                                         //Keep the motors running.
-    if (esc_4 < 1100) esc_4 = 1100;                                         //Keep the motors running.
+    if (esc_1_cmd < 1100) esc_1_cmd = 1100;                                         //Keep the motors running.
+    if (esc_2_cmd < 1100) esc_2_cmd = 1100;                                         //Keep the motors running.
+    if (esc_3_cmd < 1100) esc_3_cmd = 1100;                                         //Keep the motors running.
+    if (esc_4_cmd < 1100) esc_4_cmd = 1100;                                         //Keep the motors running.
 
-    if(esc_1 > 2000)esc_1 = 2000;                                           //Limit the esc-1 pulse to 2000us.
-    if(esc_2 > 2000)esc_2 = 2000;                                           //Limit the esc-2 pulse to 2000us.
-    if(esc_3 > 2000)esc_3 = 2000;                                           //Limit the esc-3 pulse to 2000us.
-    if(esc_4 > 2000)esc_4 = 2000;                                           //Limit the esc-4 pulse to 2000us.
+    if(esc_1_cmd > 2000)esc_1_cmd = 2000;                                           //Limit the esc-1 pulse to 2000us.
+    if(esc_2_cmd > 2000)esc_2_cmd = 2000;                                           //Limit the esc-2 pulse to 2000us.
+    if(esc_3_cmd > 2000)esc_3_cmd = 2000;                                           //Limit the esc-3 pulse to 2000us.
+    if(esc_4_cmd > 2000)esc_4_cmd = 2000;                                           //Limit the esc-4 pulse to 2000us.
   }
 
   else{
-    esc_1 = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-1.
-    esc_2 = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-2.
-    esc_3 = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-3.
-    esc_4 = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-4.
+    esc_1_cmd = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-1.
+    esc_2_cmd = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-2.
+    esc_3_cmd = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-3.
+    esc_4_cmd = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-4.
   }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -331,15 +331,15 @@ void loop(){
   if(micros() - loop_timer > 4050)digitalWrite(12, HIGH);                   //Turn on the LED if the loop time exceeds 4050us.
 
   //All the information for controlling the motor's is available.
-  //The refresh rate is 250Hz. That means the esc's need there pulse every 4ms.
+  //The refresh rate is 250Hz. That means the ESCs needs new pulses every 4ms.
   while(micros() - loop_timer < 4000);                                      //We wait until 4000us are passed.
   loop_timer = micros();                                                    //Set the timer for the next loop.
 
   PORTD |= B11110000;                                                       //Set digital outputs 4,5,6 and 7 high.
-  timer_channel_1 = esc_1 + loop_timer;                                     //Calculate the time of the faling edge of the esc-1 pulse.
-  timer_channel_2 = esc_2 + loop_timer;                                     //Calculate the time of the faling edge of the esc-2 pulse.
-  timer_channel_3 = esc_3 + loop_timer;                                     //Calculate the time of the faling edge of the esc-3 pulse.
-  timer_channel_4 = esc_4 + loop_timer;                                     //Calculate the time of the faling edge of the esc-4 pulse.
+  timer_channel_1 = esc_1_cmd + loop_timer;                                     //Calculate the time of the faling edge of the esc-1 pulse.
+  timer_channel_2 = esc_2_cmd + loop_timer;                                     //Calculate the time of the faling edge of the esc-2 pulse.
+  timer_channel_3 = esc_3_cmd + loop_timer;                                     //Calculate the time of the faling edge of the esc-3 pulse.
+  timer_channel_4 = esc_4_cmd + loop_timer;                                     //Calculate the time of the faling edge of the esc-4 pulse.
 
   //There is always 1000us of spare time. So let's do something usefull that is very time consuming.
   //Get the current gyro and receiver data and scale it to degrees per second for the pid calculations.
